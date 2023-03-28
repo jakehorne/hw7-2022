7016632dd8cd17fc94f8ed4d52f089530f142c03
jakehorne <98118891+jakehorne@users.noreply.github.com>
2023-03-28 10:34:17 -0400

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -2,10 +2,28 @@ var video;
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window")
-
+	video = document.querySelector("#player1");
+	video.removeAttribute("loop");
+	video.removeAttribute("autoplay");
 });
 
-// document.querySelector("#play").addEventListener("click", function() {
-// 	console.log("Play Video");
-// });
+document.querySelector("#play").addEventListener("click", function() {
+	console.log("Play Video");
+	video.play();
+	document.getElementById("volume").innerHTML = (video.volume * 100) + "%";
+});
 
+document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video");
+	video.pause();
+});
+
+document.querySelector("#slower").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 0.9;
+	console.log("The playback spped is now: " + video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	video.playbackRate = video.playbackRate * 1.111;
+	console.log("The playback spped is now: " + video.playbackRate);
+});
